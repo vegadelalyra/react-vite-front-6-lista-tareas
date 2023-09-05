486ed85d0a93b47d2ad8b7f0f445206d732d5b49
Vegadelalyra <lyradeperseo@gmail.com>
2023-09-05 00:33:45 -0500

    feat: create a react todo list with dynamic profile-memory

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,105 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useRef } from 'react'
 
-function App(props) {
-  const [count, setCount] = useState(0)
-  const [tareas, setTareas] = useState([{id: 1, texto: "tarea 1"}])
-  const [tarea, setTarea] = useState("");
-  const [name, setName] = useState("Ana useState");
+function App() {
+    const nombreEnMemoria =
+        JSON.parse(sessionStorage.getItem('name-react-tasks')) || 'Ana'
+    const [nombre, setNombre] = useState(nombreEnMemoria)
+    const tareasEnMemoria =
+        JSON.parse(localStorage.getItem(nombre + '-react-tasks')) || []
+    const [tareas, setTareas] = useState(tareasEnMemoria)
+    const [tarea, setTarea] = useState('')
 
-  useEffect(()=>{
-    alert("componente montado")
-  }, [])
+    useEffect(() => {
+        localStorage.setItem(nombre + '-react-tasks', JSON.stringify(tareas))
+    }, [tareas])
 
-  useEffect(()=>{
-    alert("componente actualizado")
-  }, [name])
+    useEffect(() => {
+        const actualizarTareasPorNombre =
+            JSON.parse(localStorage.getItem(nombre + '-react-tasks')) || []
+        setTareas(actualizarTareasPorNombre)
+    }, [nombre])
 
-  const handleAddTask = () => {
-    setTareas([...tareas, {id:tareas.length + 1, texto: tarea}])
-  }
+    const nameRef = useRef(null)
 
-  return (
-    <>
-      <h1>LISTA DE TAREAS</h1>
-      {
-        tareas.map(item => <div key={item.id}>{item.texto}</div>)
-      }
-      <input type='text' name='tarea' placeholder='tarea' 
-        onChange={(e)=>setTarea(e.target.value)}>
-      </input>
-      <button type='button' onClick={()=>handleAddTask()}>Agregar tarea</button>
-    </>
-  )
+    const handleNameChange = e => {
+        e.target.innerText = e.target.innerText.trim()
+        sessionStorage.setItem(
+            'name-react-tasks',
+            JSON.stringify(nameRef.current.innerText.trim())
+        )
+        setNombre(nameRef.current.innerText.trim())
+    }
+
+    const handleAddTask = () => {
+        setTareas([...tareas, { id: tareas.length + 1, texto: tarea }])
+        setTarea('')
+    }
+
+    const handleDeleteTask = id => {
+        setTareas(tareas.filter(tarea => tarea.id !== id))
+    }
+
+    return (
+        <>
+            <h1>LISTA DE TAREAS DE </h1>
+            <h1
+                contentEditable='true'
+                ref={nameRef}
+                onBlur={handleNameChange}
+                onKeyDown={e => {
+                    if (e.key != 'Enter') return
+                    document.querySelector('input').focus()
+                }}>
+                {nombre}{' '}
+            </h1>
+            <h1
+                onClick={() => {
+                    const a = document.querySelector('[contentEditable="true"]')
+                    a.focus()
+                }}>
+                {' '}
+                <i className='fa fa-pencil' />
+            </h1>
+            <br />
+            <br />
+            <br />
+            <main>
+                {' '}
+                {tareas.map(item => (
+                    <article>
+                        {' '}
+                        <div className='tareas' key={item.id}>
+                            {item.texto}{' '}
+                        </div>{' '}
+                        <i
+                            className='fa fa-trash-o'
+                            onClick={() => handleDeleteTask(item.id)}
+                        />{' '}
+                    </article>
+                ))}
+            </main>
+            <br />
+            <input
+                type='text'
+                name='tarea'
+                placeholder='tarea'
+                maxLength={17}
+                value={tarea}
+                onChange={e => setTarea(e.target.value)}
+                onKeyDown={e => {
+                    if (e.key != 'Enter') return
+                    document.querySelector('button').click()
+                }}
+            />
+            <br />
+            <br />
+            <button type='button' onClick={() => handleAddTask()}>
+                Agregar tarea
+            </button>
+        </>
+    )
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App nombre="Ana" />
+    <App />
   </React.StrictMode>,
 )
